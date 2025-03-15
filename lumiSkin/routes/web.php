34a2f9b90ad3c21808431fdd9810ecc56596344b
Manuela <mcastanof1@eafit.edu.co>
2025-03-14 21:57:38 -0500

    initial creation of category

--- lumiSkin/routes/web.php
+++ lumiSkin/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(App\Http\Controllers\CategoryController::class)->group(function (): void {
+    Route::get('/categories', 'index')->name('category.index');
+    Route::get('/categories/create', 'create')->name('category.create');
+    Route::get('/categories/{id}', 'show')->name('category.show');
+    Route::post('/categories/store', 'store')->name('category.store');
+    Route::delete('/categories/{id}/delete', 'delete')->name('category.delete');
+});
