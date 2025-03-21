95f689b56509458145c50f5ab68aea27632404b7
SaraCmque <saramanrique2016@gmail.com>
2025-03-21 13:50:53 -0500

    SkincareTest model created

--- lumiSkin/routes/web.php
+++ lumiSkin/routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::controller(App\Http\Controllers\CategoryController::class)->group(function (): void {
     Route::get('/categories', 'index')->name('category.index');
     Route::get('/categories/create', 'create')->name('category.create');
