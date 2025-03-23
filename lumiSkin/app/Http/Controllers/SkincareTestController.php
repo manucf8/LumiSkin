96b07fa9dd82ec38ad93aeb0d26f66a0c8902142
SaraCmque <saramanrique2016@gmail.com>
2025-03-23 18:45:15 -0500

    some views were improved

--- lumiSkin/app/Http/Controllers/SkincareTestController.php
+++ lumiSkin/app/Http/Controllers/SkincareTestController.php
@@ -12,37 +12,40 @@ class SkincareTestController extends Controller
 {
     protected $chatGPTService;
 
-    public function __construct(ChatGPTService $chatGPTService)
-    {
-        $this->chatGPTService = $chatGPTService;
-    }
-
-    public function getRecommendation(Request $request): JsonResponse
-    {
-        $userResponses = $request->all();
-
-        $products = Product::all();
-
-        if ($products->isEmpty()) {
-            return response()->json([
-                'recommendation' => 'Actualmente no tenemos productos en la tienda para recomendar. ¡Vuelve pronto!'
-            ]);
-        }
-
-        $recommendation = $this->chatGPTService->getRecommendationFromProducts($userResponses, $products);
-
-        return response()->json([
-            'recommendation' => $recommendation
-        ]);
-    }
-
     public function index(): View
     {   
         $viewData = [];
         $viewData['title'] = 'Skincare Recommendation Test';
         $viewData['subtitle'] = 'Fill out the form to get your personalized skincare recommendation';
 
+        $questions = json_decode(file_get_contents(public_path('data/questions.json')), true);
+        $viewData['questions'] = $questions;
+
         return view('skincare_test.index')->with('viewData', $viewData);
+    }
+
+    public function __construct(ChatGPTService $chatGPTService)
+    {
+        $this->chatGPTService = $chatGPTService;
+    }
+
+    public function getRecommendation(Request $request): View
+    {
+        $userResponses = $request->all();
+        $products = Product::all(['name', 'description', 'brand', 'price']);
+
+        if ($products->isEmpty()) {
+            $recommendation = 'We currently have no products in the store to recommend. Please check back soon!';
+        } else {
+            $recommendation = $this->chatGPTService->getRecommendationFromProducts($userResponses, $products);
+        }
+
+        $viewData = [];
+        $viewData['title'] = 'Product Recommendations';
+        $viewData['subtitle'] = 'Based on your answers, here are our product recommendations';
+        $viewData['recommendation'] = $recommendation;
+
+        return view('skincare_test.recommendation')->with('viewData', $viewData);
     }
 
 }
